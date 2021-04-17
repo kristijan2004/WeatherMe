3798bf0e3ed5d45b56cd32d1b77bc6145e06f4e4
Kiko <kristijanivanovski2004@gmail.com>
2021-04-17 21:45:49 +0200

    added weather icons, macedonian translations

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,6 +1,13 @@
 const search = document.querySelector(".search");
 const units = document.getElementById("units");
 const language = document.getElementById("language");
+
+let snow = document.querySelector(".snow");
+let thunder = document.querySelector(".thunder");
+let rain = document.querySelector(".rain");
+let sun = document.querySelector(".sun");
+let cloud = document.querySelector(".cloud");
+
 search.addEventListener("keypress", findCity);
 
 function findCity(e) {
@@ -19,27 +26,84 @@ function getResult(city, units, language) {
 }
 function displayResults(weather) {
   console.log(weather);
-  let city = document.querySelector(".location .city");
-  city.innerText = `${weather.name}, ${weather.sys.country}`;
+  if (weather.cod === "404") {
+    document.querySelector(".validName").style.opacity = "0.6";
+    setTimeout(() => {
+      document.querySelector(".validName").style.opacity = "0";
+    }, 3000);
+  } else {
+    let city = document.querySelector(".location .city");
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
 
-  let now = new Date();
-  let date = document.querySelector(".location .date");
-  date.innerText = dateBuilder(now);
+    let now = new Date();
+    let date = document.querySelector(".location .date");
+    date.innerText = dateBuilder(now);
 
-  let temp = document.querySelector(".current .temp");
-  temp.innerHTML = `${Math.round(weather.main.temp)}<span>${
-    units.value === "metric" ? "°C" : "°F"
-  }</span>`;
+    let temp = document.querySelector(".current .temp");
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>${
+      units.value === "metric" ? "°C" : "°F"
+    }</span>`;
 
-  let weatherEl = document.querySelector(".current .weather");
-  weatherEl.innerText = weather.weather[0].main;
+    let weatherEl = document.querySelector(".current .weather");
+    weatherEl.innerText = `${
+      language.value === "mk"
+        ? `${
+            weather.weather[0].main === "Clouds"
+              ? "Облаци"
+              : `${
+                  weather.weather[0].main === "Snow"
+                    ? "Снег"
+                    : `${
+                        weather.weather[0].main === "Clear"
+                          ? "Чисто"
+                          : `${
+                              weather.weather[0].main ===
+                              ("Fog" || "Mist" || "Smoke" || "Dust")
+                                ? "Магла"
+                                : `${
+                                    weather.weather[0].main === "Rain"
+                                      ? "Дожд"
+                                      : `${
+                                          weather.weather[0].main === "Drizzle"
+                                            ? "Дожд"
+                                            : `${
+                                                weather.weather[0].main ===
+                                                  "Thunderstorm" && "Грмотевици"
+                                              }`
+                                        }`
+                                  }`
+                            }`
+                      }`
+                }`
+          }`
+        : weather.weather[0].main
+    }`;
 
-  let hilow = document.querySelector(".hi-low");
-  hilow.innerText = `${Math.round(weather.main.temp_min)}${
-    units.value === "metric" ? "°C" : "°F"
-  } / ${Math.round(weather.main.temp_max)}${
-    units.value === "metric" ? "°C" : "°F"
-  }`;
+    let icon = document.querySelector(".weatherIcon");
+    icon.innerHTML = "";
+    icon.appendChild(
+      weather.weather[0].main === "Clouds"
+        ? cloud
+        : weather.weather[0].main === "Snow"
+        ? snow
+        : weather.weather[0].main === "Clear"
+        ? sun
+        : weather.weather[0].main === ("Fog" || "Mist" || "Smoke" || "Dust")
+        ? cloud
+        : weather.weather[0].main === "Rain"
+        ? rain
+        : weather.weather[0].main === "Drizzle"
+        ? rain
+        : weather.weather[0].main === "Thunderstorm" && thunder
+    );
+
+    let hilow = document.querySelector(".hi-low");
+    hilow.innerText = `${Math.round(weather.main.temp_min)}${
+      units.value === "metric" ? "°C" : "°F"
+    } / ${Math.round(weather.main.temp_max)}${
+      units.value === "metric" ? "°C" : "°F"
+    }`;
+  }
 }
 function dateBuilder(d) {
   let months = [
